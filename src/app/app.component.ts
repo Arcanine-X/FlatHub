c88b98164c2fd3e96381017f68d259c26f781496
Jesse Williams <williajess7@ecs.vuw.ac.nz>
2018-08-26 11:25:43 +1200

    Added data formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
-import { LocalitiesService } from './services/localities.service';
 import { Observable } from 'rxjs';
 import { ListingService } from './services/listing.service';
-import { map } from 'rxjs/operators';
 import { DataStorageService } from './services/data-storage.service';
 
 @Component({
@@ -13,52 +11,29 @@ import { DataStorageService } from './services/data-storage.service';
 export class AppComponent {
   title = 'flathub';
 
-  public localities: Observable<object>;
   public listings: Observable<object>;
-
-  public region: any;  
-  public districtId: any;
-  public suburb: any;
   public uniToDis = [];
+  public districtId: any;
+  public rentPerWeek: number;
 
   constructor(
-    public localitiesService: LocalitiesService,  
     public listingService: ListingService,
     public dataStorage: DataStorageService
   ) {
-      this.uniToDis.push({
-        Uni: "VUW",
-        disID :47
-      });
-      this.uniToDis.push({
-        Uni: "AUT",
-        disID :7
-      });      this.uniToDis.push({
-        Uni: "Auckland Univeristy",
-        disID :7
-      });      this.uniToDis.push({
-        Uni: "Otago Uni",
-        disID :69
-      });      this.uniToDis.push({
-        Uni: "Canterbury",
-        disID :60
-      });      this.uniToDis.push({
-        Uni: "Waikato",
-        disID :16
-      });
-    // this.localities = this.localitiesService.getLocalities()
-    // this.listings = this.listingService.getByDistrict(this.districtId);
-  }
-
-  public districtChanged () {
-    console.log("districtChanged");
-    // this.listingService.getByDistrict(this.districtId)
-    // .subscribe();
+      this.uniToDis.push(
+        {Uni: "VUW", disID :47},
+        {Uni: "AUT", disID :7},
+        {Uni: "Auckland Univeristy", disID :7},
+        {Uni: "Otago Uni", disID :69},
+        {Uni: "Canterbury", disID :60},
+        {Uni: "Waikato", disID :16}
+      )
+    this.listings = this.listingService.getByDistrict(this.districtId);
   }
 
   public search() {
     this.dataStorage.getFlatInfo(this.districtId)
-    .subscribe()
-    //this.districtChanged();
+    .subscribe();
+    console.log(this.dataStorage.getFlatData());
   }
 }
